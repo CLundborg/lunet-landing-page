4c4e851d2ee285248269367833d10a2c13e6b4e7
Nikolaj-Gunge <Nikolaj.Gunge@akqa.com>
2025-12-11 16:27:57 +0100

    Add logo image to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main style={{ textAlign: 'center', padding: '50px' }}>
       <h1>Welcome to Our Landing Page</h1>
       <p>We are launching soon!</p>
+      <Image src="/logo.png" alt="Logo" width={100} height={100} />
     </main>
   );
 }
